6a8a56fe21e42de9adb84a623247e05aadc9df68
AlexTrenev <165272996+AlexTrenev@users.noreply.github.com>
2026-03-26 20:06:37 +0100

    Adjusting sidebar

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "trenev.xyz",
+    pageTitle: "Trenev",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
